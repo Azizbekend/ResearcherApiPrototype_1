d59ae45b42221a650d8acbd8d8ce841d93ee5c33
Азизбек <azizma3105@gmail.com>
2025-12-16 21:49:41 +0300

    full opdate

--- ResearcherApiPrototype_1/Models/HardwareSchemaImage.cs
+++ ResearcherApiPrototype_1/Models/HardwareSchemaImage.cs
@@ -19,5 +19,8 @@ namespace ResearcherApiPrototype_1.Models
         public int? FileId { get; set; }
         public FileModel? File { get; set; }
         public int HardwareId { get; set; }
+        public int RedFileId { get; set; }
+        public int GreenFileId { get; set; }
+
     }
 }
