f7e030f17424d282a2c0b20523db1859ee888560
Azizbekend <azizma3105@gmail.com>
2025-12-23 17:20:14 +0300

    asd

--- ResearcherApiPrototype_1/Repos/ShemaRepo/SchemaRepo.cs
+++ ResearcherApiPrototype_1/Repos/ShemaRepo/SchemaRepo.cs
@@ -86,7 +86,7 @@ namespace ResearcherApiPrototype_1.Repos.ShemaRepo
                     Left = item.Left,
                     Top = item.Top,
                     NodeInfoId = nodeInfo.Id,
-                    SchemeId = nodeInfo.SchemeId,
+                    SchemeId = item.SchemeId,
                     NodeName = nodeInfo.Name,
                     MeasurementName = nodeInfo.Mesurement
                 };
