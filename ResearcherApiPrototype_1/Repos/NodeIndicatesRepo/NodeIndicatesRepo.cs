188136194e33fbfd0a2872b28acbcef5f6ff4459
Azizbekend <azizma3105@gmail.com>
2026-01-12 15:48:00 +0300

    asd

--- ResearcherApiPrototype_1/Repos/NodeIndicatesRepo/NodeIndicatesRepo.cs
+++ ResearcherApiPrototype_1/Repos/NodeIndicatesRepo/NodeIndicatesRepo.cs
@@ -110,17 +110,13 @@ namespace ResearcherApiPrototype_1.Repos.NodeIndicatesRepo
            var hourP = await _appDbContext.NodesIndicates.Where(x => x.PlcNodeId.EndsWith("HMI_AI_FQIR1.hPV")).OrderByDescending(x => x.Id).FirstOrDefaultAsync();
 
             Console.WriteLine($"=== LOG: ElectroConsumption Calculation ===");
-            Console.WriteLine($"el1.Indicates: {(el1?.Indicates ?? "NULL")}");
-            Console.WriteLine($"el2.Indicates: {(el2?.Indicates ?? "NULL")}");
+            Console.WriteLine($"el1.Indicates: {(el1.Indicates)}");
+            Console.WriteLine($"el2.Indicates: {(el2.Indicates)}");
 
             dto.HourEfficiency = hourP.Indicates;
             dto.ElectroConsumption = Math.Round((((double.Parse(el1.Indicates) + double.Parse(el2.Indicates))) / 1000), 2);
 
-            Console.WriteLine($"el1 parsed: {el1Value}");
-            Console.WriteLine($"el2 parsed: {el2Value}");
-            Console.WriteLine($"Sum: {sum}");
-            Console.WriteLine($"Result (sum/1000): {result}");
-
+            Console.WriteLine($"Sum: {dto.ElectroConsumption}");
 
             dto.WaterConsumption = ((double.Parse(water1.Indicates) + double.Parse(water2.Indicates)) / 1000).ToString();
             var list = await _appDbContext.NodesIndicates.Where(x => x.PlcNodeId.EndsWith("HMI_AI_FQIR1.hPV") && x.TimeStamp == DateTime.Today.AddDays(-1).ToUniversalTime()).ToListAsync();
